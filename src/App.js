cb717746c63c55f9c91e5bb73d9d59e647539ad1
claremburu <mburuclare8@gmail.com>
2022-01-24 23:32:49 +0300

    Updated Readme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import './index.css';
 
 import { marked } from "marked";
 
@@ -6,14 +7,7 @@ const TEST_MARKDOWN =
   "# Markdown Text goes here\n## You can also make subheadings\n\nOne of the **trickiest** parts of getting this project to work was learning how to use `dangerouslySetInnerHTML` to make the previewer display the output of [marked.js](https://github.com/markedjs/marked/blob/master/README.md) as HTML instead of a string.\n\nAccording to the React Documentation,\n> dangerouslySetInnerHTML is React’s replacement for using innerHTML in the browser DOM. In general, setting HTML from code is risky because it’s easy to inadvertently expose your users to a cross-site scripting (XSS) attack.\n\nExample Code:\n```\nfunction createMarkup() {\n  return {__html: 'First &middot; Second'};\n}\n\nfunction MyComponent() {\n  return <div dangerouslySetInnerHTML={createMarkup()} />;\n}\n```\n\nJust remember to:\n- Search, Read, Ask\n- Ask for help on the Forum (that's what worked for me.)\n\n";
 
 function App() {
-  const [parsedMarkdown, setParsedMarkdown] = React.useState(TEST_MARKDOWN);
-
-  React.useEffect(() => {
-    setParsedMarkdown(marked(parsedMarkdown));
-    return () => {
-      setParsedMarkdown(TEST_MARKDOWN);
-    };
-  }, [parsedMarkdown]);
+  const [parsedMarkdown, setParsedMarkdown] = useState('');
 
   const handleChange = (event) => {
     const markdown = event.target.value;
@@ -25,23 +19,29 @@ function App() {
     setParsedMarkdown(html);
   };
 
+  useEffect(() => {
+    setParsedMarkdown((parsedMarkdown));
+    // return () => {
+    //   setParsedMarkdown(TEST_MARKDOWN);
+    // };
+  }, [parsedMarkdown]);
+
   return (
-    <div className="div-background">
+    <div className="flex m-10">
       <textarea
         id="editor"
         cols={30}
         rows={10}
-        className="editor hero is-fullheight"
+        className="w-1/2 m-5"
         onChange={handleChange}
       >
         {TEST_MARKDOWN}
       </textarea>
-      <pre
-        className="previewer"
+      <div
+        className="w-1/2 m-5 p-5 bg-slate-900 text-white text-lg"
         id="preview"
-        className="previewer hero is-fullheight"
         dangerouslySetInnerHTML={{ __html: parsedMarkdown }}
-      ></pre>
+      ></div>
     </div>
   );
 }
